14ce2eaf061aed29416bbe39452c0a6272061acf
Centripio <code@centripio.io>
2020-02-09 16:36:36 -0800

    [togglz-payment] Returning object CC Payment Service.

--- src/main/java/com/centripio/togglz/payment/service/CCPaymentService.java
+++ src/main/java/com/centripio/togglz/payment/service/CCPaymentService.java
@@ -5,12 +5,14 @@ import com.centripio.togglz.payment.dto.PaymentRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Slf4j
 @Service
 public class CCPaymentService implements IPaymentService {
     @Override
     public PaymentConfirmation doPayment(PaymentRequest request) {
         log.info("Calling CC Payment Service..");
-        return null;
+        return new PaymentConfirmation(UUID.randomUUID().toString());
     }
 }
